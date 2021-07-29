f974bb647aa9d45dbe64b5ce7e9c0b2275390e03
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-07-29 20:09:53 +0530

    Added Zomato Project Models

---
+++ server/Database/food/index.js
@@ -0,0 +1,29 @@
+import mongoose, { mongo } from "mongoose";
+
+const FoodSchema = new mongoose.Schema({
+    name: {type:String, required: true},
+    description: {type:String, required: true},
+    isVeg: {type: Boolean, required: true},
+    isContainsEgg: {type: Boolean, required: true},
+    category: {type: String, required: true},
+    photos: {
+        type: mongoose.Types.ObjectId,
+        ref: "Images",
+    },
+    price: {type: Number, default: 150, required: true},
+    addOns: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "Foods",
+        },
+    ],
+    restaurants: {
+        
+        type: mongoose.Types.ObjectId,
+        ref: "Restaurants",
+        required: true,
+        
+    },
+});
+
+export const FoodModel = mongoose.model("Foods", FoodSchema);
