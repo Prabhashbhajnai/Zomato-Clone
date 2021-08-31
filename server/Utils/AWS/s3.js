51412980ea7dc5819a0f53ced8cf2ca39c770c9f
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-08-31 13:07:33 +0530

    major tweaking

--- server/Utils/AWS/s3.js
+++ server/Utils/AWS/s3.js
@@ -2,8 +2,8 @@ import AWS from "aws-sdk";
 
 // AWS s3 bucket config
 const s3Bucket = new AWS.S3({
-  accessKeyId: AWS_S3_ACCESS_KEY,
-  secretAccessKey: AWS_S3_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: "ap-south-1",
 });
 
