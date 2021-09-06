e95de285f9bcce7e25eee76590cb11e1a4828cd3
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-09-06 18:02:55 +0530

    added dining carousal

---
+++ client/src/Components/Dining/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// components
+import DiningCarousal from './DiningCarousal';
+
+const Dining = () => {
+    return (
+        <>
+            <DiningCarousal />
+        </>
+    );
+};
+
+export default Dining
--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -3,12 +3,14 @@ import { useParams } from 'react-router-dom';
 
 // components
 import Delivery from './Delivery';
+import Dining from './Dining';
 
 const Master = () => {
     const {type} = useParams();
     return (
         <div className="my-5">
             {type === "delivery" && <Delivery />}
+            {type === "dining" && <Dining />}
         </div>
     );
 };
