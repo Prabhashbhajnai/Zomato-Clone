063dd670eb68a881f56dcbdf715aa77f607dde61
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-09-07 16:22:17 +0530

    fixed redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
+import { Route, Redirect } from "react-router-dom";
+
 // HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
 
 // Component
 import Temp from "./Components/temp";
-import Master from "./Components/master";
+
+// pages
+import Home from "./Page/Home";
 
 function App() {
-  return <>
-    <HomeLayoutHOC path="/" exact component={Temp}/>
-    <HomeLayoutHOC path="/:type" exact component={Master}/>
-
-  </>
-}
+  return(
+    <>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <HomeLayoutHOC path="/:type" exact component={Home}/>
+    </>
+  );
+};
 
 export default App;
