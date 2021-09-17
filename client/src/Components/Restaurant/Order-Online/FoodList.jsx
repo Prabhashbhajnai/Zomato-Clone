e494270c82c890bad16b67dabd89a106d8f53bf7
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-09-17 22:42:22 +0530

    added food list

---
+++ client/src/Components/Restaurant/Order-Online/FoodList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import FoodItem from './FoodItem';
+
+const FoodList = (props) => {
+    return (
+        <>
+            <div>
+                <h2 className="bg-white top-0 w-full px-2 py-1 z-10 sticky text-xl font-semibold">{props.title}</h2>
+                <div className="flex flex-col gap-3 md:gap-5">
+                    {props.items.map((item) => (
+                        <FoodItem
+                            {...item}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default FoodList;
--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -7,32 +7,45 @@ import {BiTimeFive} from 'react-icons/bi';
 import FloatMenuButton from '../../Components/Restaurant/Order-Online/FloatMenuButton';
 import MenuListContainer from '../../Components/Restaurant/Order-Online/MenuListContainer';
 import FoodItem from '../../Components/Restaurant/Order-Online/FoodItem';
+import FoodList from '../../Components/Restaurant/Order-Online/FoodList';
 
 const OrderOnline = () => {
     return (
         <>
-            <div className="w-full flex">
+            <div className="w-full h-screen flex">
                 <aside className="hidden md:flex flex-col gap-3 border-r-2 overflow-y-scroll border-gray-200 h-screen w-64 mr-10">
                     <MenuListContainer />
                     <MenuListContainer />
                     <MenuListContainer />
                     <MenuListContainer />
                 </aside>
-                <div className="w-full md:w-4/6">
-                    <h2 className="text-xl font-semibold">Order Online</h2>
-                    <h4 className="flex items-center gap-2 font-light text-gray-500">
-                        <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 Min
-                    </h4>
+                <div className="w-full h-screen px-3 md:w-4/6">
+                    <div className="pl-3 mb-4">
+                        <h2 className="text-xl font-semibold">Order Online</h2>
+                        <h4 className="flex items-center gap-2 font-light text-gray-500">
+                            <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 Min
+                        </h4>
+                    </div>
 
-                    <section>
-                        <FoodItem 
-                            image="https://b.zmtcdn.com/data/dish_photos/261/1816eba714ae23f5cbde48d7e9086261.jpg"
-                            title="Mutton Handi [Half]"
-                            price="1000"
-                            rating={3}
-                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quae l
-                            aborum aliquam iusto cupiditate quisquam fugiat quidem eligendi, maxime officiis eum qui. Aperia
-                            m assumenda laboriosam libero rem doloribus, consequatur maiores!"
+                    <section className="flex flex-col h-screen overflow-y-scroll gap-3 md:gap-5">
+                        <FoodList 
+                            title="Recommended" 
+                            items={[
+                                {
+                                    title: "Mutton Handi [Half]",
+                                    price: "1000",
+                                    rating: 3,
+                                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quae laborum aliquam iusto cupiditate quisquam fugiat quidem eligendi, maxime officiis eum qui. Aperiam assumenda laboriosam libero rem doloribus, consequatur maiores!",
+                                    image: "https://b.zmtcdn.com/data/dish_photos/261/1816eba714ae23f5cbde48d7e9086261.jpg"
+                                },
+                                {
+                                    title: "Mutton Handi [Half]",
+                                    price: "1000",
+                                    rating: 3,
+                                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quae laborum aliquam iusto cupiditate quisquam fugiat quidem eligendi, maxime officiis eum qui. Aperiam assumenda laboriosam libero rem doloribus, consequatur maiores!",
+                                    image: "https://b.zmtcdn.com/data/dish_photos/261/1816eba714ae23f5cbde48d7e9086261.jpg"
+                                },
+                            ]} 
                         />
                     </section>
                 </div>
