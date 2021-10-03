ce1c2ce6c5fea98a5f271fa6c23f4b520e3100e6
Prabhashbhajani <prabhashbhajani@gmail.com>
2021-10-03 20:21:04 +0530

    added menu and photos

--- client/src/Page/Restaurant/Menu.jsx
+++ client/src/Page/Restaurant/Menu.jsx
@@ -1,18 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// redux action
+import { getImage } from '../../Redux/Reducer/Image/Image.action';
 
 // component
 import MenuCollection from '../../Components/Restaurant/MenuCollection';
 
 const Menu = () => {
-
     const [menus, setMenus] = useState([]);
+    
+    const reduxState = useSelector(
+        (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+    );
+    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(reduxState) {
+            dispatch(getImage(reduxState?.menuImage)).then((data) => {
+                const images = [];
+                data.payload.image.images.map(({location}) => images.push(location))
+                setMenus(images)
+            });
+        }
+    }, [reduxState]);
 
     return (
         <>
             <div className="flex flex-wrap gap-3">
-                {menus.map((menu) => (
-                    <MenuCollection {...menu} />
-                ))}
+                <MenuCollection menuTitle="Menu" pages={menus.length} image={menus} />
             </div>
         </>
     );
--- client/src/Page/Restaurant/Photos.jsx
+++ client/src/Page/Restaurant/Photos.jsx
@@ -1,22 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import ImageViewer from 'react-simple-image-viewer';
+
+// redux action
+import { getImage } from '../../Redux/Reducer/Image/Image.action';
 
 // component
 import PhotoCollection from '../../Components/Restaurant/PhotosCollection';
 
 const Photos = () => {
 
-    const [photos, setPhotos] = useState([
-        "https://b.zmtcdn.com/data/pictures/chains/4/3300424/b3055069ad7ce4cd707acff1449c3f78.jpg", 
-        "https://b.zmtcdn.com/data/pictures/chains/4/3300424/bcc941604e5dd4f94954e26e460695dc.jpg", 
-        "https://b.zmtcdn.com/data/pictures/chains/4/3300424/10782846b5908f706f193d026b70af3c.jpg", 
-        "https://b.zmtcdn.com/data/pictures/chains/4/3300424/7ed8023847cf71ee206b2d04c136576b.jpg", 
-        "https://b.zmtcdn.com/data/pictures/chains/4/3300424/2d50cb2965e522e5db9f23f5c6eef1d3.jpg", 
-        "https://b.zmtcdn.com/data/pictures/chains/4/3300424/da2592e0d25f079cbf9aedf436cabf6f.jpg", 
-        "https://b.zmtcdn.com/data/pictures/chains/4/3300424/10782846b5908f706f193d026b70af3c.jpg"
-    ]);
+    const [photos, setPhotos] = useState([]);
     const [isMenuOpen,  setIsMenuOpen] = useState(false);
     const [CurrentImg,  setCurrentImg] = useState(0);
+
+    const reduxState = useSelector(
+        (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+    );
+    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(reduxState) {
+            dispatch(getImage(reduxState?.photos)).then((data) => {
+                const images = [];
+                data.payload.image.images.map(({location}) => images.push(location))
+                setPhotos(images)
+            });
+        }
+    }, [reduxState]);
+
     const closeViewer = () => setIsMenuOpen(false);
     const openViewer = () => setIsMenuOpen(true);
 
